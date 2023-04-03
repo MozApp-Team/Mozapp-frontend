27b5cb8f332b984dea77ca6e3c96e4cdea5e4600
David Vu <davidvu7887@gmail.com>
2023-04-03 14:32:53 -0700

    Fixing gh pages routing

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <React.StrictMode>
-    <Router>
+  <Router>
+    <React.StrictMode>
       <App />
-    </Router>
-  </React.StrictMode>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
