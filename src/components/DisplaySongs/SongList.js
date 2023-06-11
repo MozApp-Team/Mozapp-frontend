4452f0a0ab92fbafdd7cadefc713419cb59d2a23
Adrian Bao <adrianb5275@gmail.com>
2023-06-11 13:48:30 -0700

    working skip back track functionality

---
+++ src/components/DisplaySongs/SongList.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import './SongList.css';
+
+const SongList = ({ metadata }) => 
+{
+  const [selectedSong, setSelectedSong] = useState(null);
+  const [songsPlayed, setSongsPlayed] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  const handlePlayButtonClick = (song) => 
+  {
+    setSelectedSong(song);
+    setSongsPlayed((prevSongs) => [...prevSongs, song]);
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const onClickNext = () => 
+  {
+    const randomIndex = Math.floor(Math.random() * metadata.length);
+    const randomSong = metadata[randomIndex];
+    setSelectedSong(randomSong);
+    setSongsPlayed((prevSongs) => [...prevSongs, randomSong]);
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const onClickPrevious = () => 
+  {
+    if (currentIndex > 0) 
+    {
+      const previousIndex = currentIndex - 1;
+      const previousSong = songsPlayed[previousIndex];
+      setSelectedSong(previousSong);
+      setCurrentIndex(previousIndex);
+    }
+  };
+
+  return (
+    <div className="song-list-container">
+      <h1>Song List</h1>
+      <div className="song-table">
+        {metadata.map((song) => 
+        (
+          <div key={song.id} className="song-row">
+            <div className="album-art">
+              <button onClick={() => handlePlayButtonClick(song)}>
+                <img src={song.cover_image} alt={song.title} />
+              </button>
+            </div>
+            <div className="song-info">
+              <h3>{song.title}</h3>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <strong>Artist:</strong>
+                    </td>
+                    <td>{song.artist}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Album:</strong>
+                    </td>
+                    <td>{song.album}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Genre:</strong>
+                    </td>
+                    <td>{song.genre}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Year:</strong>
+                    </td>
+                    <td>{song.release_year}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Format:</strong>
+                    </td>
+                    <td>{song.format}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ))}
+      </div>
+      {selectedSong && 
+      (
+        <div className="music-player">
+          <h2>Now Playing: {selectedSong.title}</h2>
+          <AudioPlayer
+            autoPlay
+            src={`http://localhost:8081/music/${selectedSong.album}/${selectedSong.title}.mp3`}
+            showSkipControls={true}
+            onClickPrevious={onClickPrevious}
+            onClickNext={onClickNext}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SongList;
